7e886885459f273fc4b9ecad48e888a5079e2468
sho ochiai <ochi1020@gmail.com>
2021-01-31 12:30:11 +0900

    feat: 完了チェックボックスの作成（動作未確認）

--- src/component/TodoList.tsx
+++ src/component/TodoList.tsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { ChangeEvent } from "react";
 import { Task } from "../domain/Task";
 
 export function TodoList(props: Props) {
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>, task: Task) => {
+    props.doComplete(event, task);
+  }
 
   const todoList = () => {
     return props.tasks.map(task => {
       return(
       <div key={task.title}>
         <span>{task.title}</span>
-        <input type="checkbox" defaultChecked={task.done}/>
+        <input type="checkbox" defaultChecked={task.done} onChange={(e) => onChange(e, task)}/>
       </div>
       )
     })
@@ -19,5 +23,6 @@ export function TodoList(props: Props) {
 }
 
 type Props = {
-  tasks: Task[]
+  tasks: Task[],
+  doComplete: (event: ChangeEvent<HTMLInputElement>, task: Task) => void
 }
