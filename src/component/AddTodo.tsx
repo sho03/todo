ab6b097e19f7880e8731537bd953b2804dd95070
Sho Ochiai <ochiai_s@welmo.co.jp>
2021-02-08 21:06:11 +0900

    feat: 空文字のタスクは登録できないように修正

--- src/component/AddTodo.tsx
+++ src/component/AddTodo.tsx
@@ -4,6 +4,7 @@ import { Task } from "../domain/Task";
 export function AddTodo(props: Props) {
 
   const [inputValue, setValue] = useState('');
+  const [disabled, setDisabled] = useState(true);
 
   const addNewTodo = () => {
     const newTask = new Task(inputValue);
@@ -11,9 +12,18 @@ export function AddTodo(props: Props) {
     setValue('');
   }
 
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+    if (e.target.value) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }
+
   return <div>
-    <input type="text" value={inputValue} onChange={(e) => setValue(e.target.value)}/>
-    <button onClick={addNewTodo}>追加</button>
+    <input type="text" value={inputValue} onChange={onChange} />
+    <button onClick={addNewTodo} disabled={disabled}>追加</button>
   </div>
 };
 
